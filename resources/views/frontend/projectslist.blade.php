5af9be33a760f7ae0ed7ecf266c6a93931fec2ea
Kavindu Theekshana <kavindutheekshana@gmail.com>
2024-01-07 17:35:43 +0000

    Bug Fix V1.2.1

--- resources/views/frontend/projectslist.blade.php
+++ resources/views/frontend/projectslist.blade.php
@@ -7,28 +7,24 @@
         @include('frontend.components.header')
         <main>
             <div class="page-content">
-              <div class="project-page">
-                  <div class="project-list-area project-list-area_content-page">
-                    <h2 class="fw-bolder">{{$project_name}}</h2>
-                    <ul class="project-list-area__list">
-                        @foreach ($projects as $project)
-                        <li class="project-list-area__list-item">
-                        <a href="{{ route('project.show', ['slug' => $project->slug]) }}" class="project-list-area__list-item-link">
-                          <h4 class="fw-bolder m-0">{{$project->title}}</h4>
-                          <p class="m-0">Location _ {{$project->location}}</p>
-                          <p>Land Extent_{{$project->land}}p/Floor area_{{$project->floor}} Sqft/ Project year_{{$project->project_year}}/ Completed_{{$project->completed_year}}</p>
-                        </a>
-                      </li>
-                @endforeach
-
-
-
-                    </ul>
-                  </div>
-              </div>
+                <div class="project-page">
+                    <div class="project-list-area project-list-area_content-page">
+                        <h2 class="fw-bolder">{{ $project_name }}</h2>
+                        <ul class="project-list-area__list">
+                            @foreach ($projects as $project)
+                                <li class="project-list-area__list-item">
+                                    <a href="{{ route('project.show', ['slug' => $project->slug]) }}"
+                                        class="project-list-area__list-item-link">
+                                        <h4 class="fw-bolder m-0">{{ $project->title }}</h4>
+                                        <p class="m-0">Location _ {{ $project->location }}</p>
+                                        <p>Scale - {{ $project->scale }} / Stage{{ $project->stage }}</p>
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
             </div>
-          </main>
+        </main>
     </body>
 @endsection
-
-
